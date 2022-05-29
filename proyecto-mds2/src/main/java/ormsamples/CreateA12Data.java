33aaa2c846a66c885aefc2ada55725e488021629
Jesús Cazorla González <jcg449@inlumine.ual.es>
2022-05-29 14:07:10 +0200

    Ejemplos de crear en Samples

--- proyecto-mds2/src/main/java/ormsamples/CreateA12Data.java
+++ proyecto-mds2/src/main/java/ormsamples/CreateA12Data.java
@@ -5,6 +5,26 @@
 package ormsamples;
 
 import org.orm.*;
+
+import basededatos.A12PersistentManager;
+import basededatos.Album;
+import basededatos.AlbumDAO;
+import basededatos.Artista;
+import basededatos.ArtistaDAO;
+import basededatos.Cancion;
+import basededatos.CancionDAO;
+import basededatos.Estilo;
+import basededatos.EstiloDAO;
+import basededatos.Evento;
+import basededatos.EventoDAO;
+import basededatos.Horas;
+import basededatos.HorasDAO;
+import basededatos.Lista_Reproduccion;
+import basededatos.Lista_ReproduccionDAO;
+import basededatos.Usuario;
+import basededatos.UsuarioDAO;
+import basededatos.Usuario_Registrado;
+import basededatos.Usuario_RegistradoDAO;
 public class CreateA12Data {
 	public void createTestData() throws PersistentException {
 		PersistentTransaction t = basededatos.A12PersistentManager.instance().getSession().beginTransaction();
@@ -50,18 +70,142 @@ public class CreateA12Data {
 		
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws PersistentException {
+//		try {
+//			CreateA12Data createA12Data = new CreateA12Data();
+//			try {
+//				createA12Data.createTestData();
+//			}
+//			finally {
+//				basededatos.A12PersistentManager.instance().disposePersistentManager();
+//			}
+//		}
+//		catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		PersistentTransaction t = A12PersistentManager.instance().getSession().beginTransaction();
 		try {
-			CreateA12Data createA12Data = new CreateA12Data();
-			try {
-				createA12Data.createTestData();
-			}
-			finally {
-				basededatos.A12PersistentManager.instance().disposePersistentManager();
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
+			// TODO Initialize the properties of the persistent object here, the following properties must be initialized before saving : contiene_cancion, autor
+			//CREADOR ALBUM
+//			Album album = AlbumDAO.createAlbum();
+//			album.setTitutloAlbum("Prueba");
+//			album.setFechaEdicion("29/05/2022");
+//			album.contiene_cancion.add(CancionDAO.getCancionByORMID(2));
+//			album.autor.add(ArtistaDAO.getArtistaByORMID(2));
+//			AlbumDAO.save(album);						
+//		
+			// TODO Initialize the properties of the persistent object here, the following properties must be initialized before saving : seguidor_usuario, horass, Ultimo_exito, favorita, recibe_notificacion, seguido, propietario, seguir, tipoUsuario
+			//CREADOR USUARIO - REGISTRADO O ARTISTA 	
+		
+//			int tipoUsuario = 1;			
+//			if(tipoUsuario == 1) {
+//				Usuario_Registrado usuario = Usuario_RegistradoDAO.createUsuario_Registrado();
+//				usuario.setNombre("Jesus");
+//				usuario.setContrasena("password");
+//				usuario.setCorreo("jcg449@inlumine.ual.es");
+//				usuario.setFoto("fotoDefault.jpg");
+//				usuario.setTipoUsuario(1);
+//				usuario.seguidor_usuario.add(usuario);
+//				usuario.horass.add(HorasDAO.getHorasByORMID(2));
+//				usuario.ultimo_exito.add(CancionDAO.getCancionByORMID(1));
+//				usuario.favorita.add(CancionDAO.getCancionByORMID(1));
+//				usuario.recibe_notificacion.add(EventoDAO.getEventoByORMID(2));
+//				usuario.seguido.add(usuario);
+//				Lista_Reproduccion lr = Lista_ReproduccionDAO.createLista_Reproduccion();
+//				lr.seguidor.add(usuario);
+//				lr.setAutor(usuario);
+//				Lista_ReproduccionDAO.save(lr);
+//				usuario.propietario.add(lr);
+//				usuario.seguir.add(lr);
+//				Usuario_RegistradoDAO.save(usuario);
+//			}else if(tipoUsuario == 2) {
+//				Artista usuario = ArtistaDAO.createArtista();
+//				usuario.setNombre("Jesus");
+//				usuario.setContrasena("password");
+//				usuario.setCorreo("jcg449@inlumine.ual.es");
+//				usuario.setFoto("fotoDefault.jpg");
+//				usuario.setTipoUsuario(1);
+//				usuario.seguidor_usuario.add(usuario);
+//				usuario.horass.add(HorasDAO.getHorasByORMID(2));
+//				usuario.ultimo_exito.add(CancionDAO.getCancionByORMID(1));
+//				usuario.favorita.add(CancionDAO.getCancionByORMID(1));
+//				usuario.recibe_notificacion.add(EventoDAO.getEventoByORMID(2));
+//				usuario.seguido.add(usuario);
+//				Lista_Reproduccion lr = Lista_ReproduccionDAO.createLista_Reproduccion();
+//				
+//				lr.seguidor.add(usuario);
+//				lr.setAutor(usuario);
+//				Lista_ReproduccionDAO.save(lr);
+//				
+//				usuario.propietario.add(lr);
+//				usuario.seguir.add(lr);
+				
+		
+			
+			//LISTA DE REPRODUCCION
+//			Lista_Reproduccion lr = Lista_ReproduccionDAO.createLista_Reproduccion();
+//			// TODO Initialize the properties of the persistent object here, the following properties must be initialized before saving : contiene_cancion, autor, seguidor
+//
+//			lr.setAutor(UsuarioDAO.getUsuarioByORMID(2));
+//			lr.setNombreLista("Prueba");
+//			lr.contiene_cancion.add(CancionDAO.getCancionByORMID(2));
+//			lr.seguidor.add(UsuarioDAO.getUsuarioByORMID(2));
+//			Lista_ReproduccionDAO.save(lr);
+//			
+			
+		
+			//ESTILO
+			// TODO Initialize the properties of the persistent object here, the following properties must be initialized before saving : cantado_por
+//
+//			Estilo estilo = EstiloDAO.createEstilo();
+//			estilo.setNombre("Pop");
+//			estilo.cantado_por.add(ArtistaDAO.getArtistaByORMID(2));
+//			EstiloDAO.save(estilo);
+		
+			//EVENTO
+			// TODO Initialize the properties of the persistent object here, the following properties must be initialized before saving : genera_notificacion, creado_por
+
+			
+//			Evento evento = EventoDAO.createEvento();
+//			evento.setCreado_por(ArtistaDAO.getArtistaByORMID(2));
+//			evento.genera_notificacion.add(UsuarioDAO.getUsuarioByORMID(2));
+//			evento.setFecha("29/05/2022");
+//			evento.setNombre("Prueba");
+//			evento.setTipoEvento("Concierto");
+//			EventoDAO.save(evento);
+//		
+			
+			//CANCION
+			// TODO Initialize the properties of the persistent object here, the following properties must be initialized before saving : Administrado_por, favorita_de, es_de, contendor_cancion, cancion_de, horass
+//
+//			Cancion cancion = CancionDAO.createCancion();
+//			Usuario aux = UsuarioDAO.getUsuarioByORMID(2);
+//			cancion.setTitulo("Prueba");
+//			cancion.setTituloCreditos("Prueba" + "\n" + "Prueba");					
+//			cancion.setCompositores("Prueba");
+//			cancion.setIntepretes("Prueba");
+//			cancion.setProductores("Prueba");
+//			cancion.setArchivoMultimedia("Prueba");
+//			cancion.setAdministrado_por(aux);
+//			cancion.favorita_de.add(aux);
+//			cancion.es_de.add(ArtistaDAO.getArtistaByORMID(2));
+//			cancion.setContendor_cancion(Lista_ReproduccionDAO.getLista_ReproduccionByORMID(2));				
+//			cancion.setCancion_de(AlbumDAO.getAlbumByORMID(2));			
+//			
+//			Horas h = HorasDAO.createHoras();		
+//			cancion.horass.add(h);
+//			h.setCancion(cancion);
+//			h.setUsuario(aux);			
+//			CancionDAO.save(cancion);
+//			
+//			
+//			HorasDAO.save(h);
+		
+			
+			
+			t.commit();
+		} catch (Exception e) {
+			t.rollback();
 		}
 	}
 }
